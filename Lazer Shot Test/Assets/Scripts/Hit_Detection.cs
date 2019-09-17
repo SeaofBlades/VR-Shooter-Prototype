6adf06135fc0769a74d13464a96786e2cbdd84ad
Matt Myers <mrmyers1@fullsail.edu>
2019-09-17 12:51:10 -0400

    Inital content push

---
+++ Lazer Shot Test/Assets/Scripts/Hit_Detection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Hit_Detection : MonoBehaviour
+{
+    public TextMeshPro Text;
+    int numhits = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+        if (collision.gameObject.tag == "Target")
+        {
+            numhits++;
+            Text.text = "Hits: " + numhits.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ Lazer Shot Test/Assets/Scripts/VR_Inputs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VR_Inputs : MonoBehaviour
+{
+    public AudioSource Gunshot;
+    public Animator Gun_ani;
+    public GameObject Bullet, MuzzlePoint, muzzleflash;
+
+    void Update()
+    {
+        Gun_ani.SetFloat("Trigger", SteamVR_Actions.default_Squeeze.GetAxis(SteamVR_Input_Sources.RightHand));
+        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+        {
+            Gun_ani.SetTrigger("Fire");
+            Gunshot.Play();
+            var bulllet = Instantiate(Bullet, MuzzlePoint.transform.position, MuzzlePoint.transform.rotation);
+            var flash = Instantiate(muzzleflash, MuzzlePoint.transform.position, MuzzlePoint.transform.rotation);
+            bulllet.GetComponent<Rigidbody>().AddForce(MuzzlePoint.transform.forward * 1000);
+            Destroy(flash.gameObject, 0.5f);
+            Destroy(bulllet.gameObject, 3.0f);
+        }
+    }
+}
